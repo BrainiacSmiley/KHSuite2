096b636c43cfdca8938ad1c3bcbabe7de59c6fef
Brainiac <Brainiac.Smily@gmx.net>
2011-12-23 16:23:27 +0100

    Initial Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KHSuite2::Application.config.secret_token = 'd768585497d086822172543ba56412ae7517c9224455c6078cf43bd89a47c5ba79b53e665d7a4c790063551df65d5d85798f38995a008377e1387be24b9de916'
